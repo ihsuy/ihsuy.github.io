e3797cc9edfb9a0c7e0b6466c362ed010e3b73d5
Yushi Qiu <jooehisaishi@gmail.com>
2021-07-24 02:24:56 +0900

    added job profile / linkedin botton etcç

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -39,16 +39,11 @@ function generateGithubCalendarTheme(theme) {
     grade1: "hsl(183.7, 36%, 80%)",
     grade0: "#eee",
   };
-  if (theme === 'spring')
-    return springTheme;
-  else if (theme === 'summer')
-    return summerTheme;
-  else if (theme === 'autumn')
-    return autumnTheme;
-  else if (theme === 'winter')
-    return winterTheme;
-  else
-    return NaN;
+  if (theme === "spring") return springTheme;
+  else if (theme === "summer") return summerTheme;
+  else if (theme === "autumn") return autumnTheme;
+  else if (theme === "winter") return winterTheme;
+  else return NaN;
 }
 
 class About extends Component {
@@ -58,43 +53,38 @@ class About extends Component {
       <React.Fragment>
         <h1>
           Yushi Qiu
-          <a className="link" href="https://github.com/ihsuy">
+          <a className="link" href="https://github.com/ihsuy" target="_blank" rel="noopener noreferrer">
             <i
               className="fa fa-github"
               aria-hidden="true"
               style={{ marginLeft: "15pt" }}
             ></i>
           </a>
+          <a className="link" href="https://www.linkedin.com/in/yushi-q-a4b431204" target="_blank" rel="noopener noreferrer">
+            <i
+              className="fa fa-linkedin"
+              aria-hidden="true"
+              style={{ marginLeft: "15pt" }}
+            ></i>
+          </a>
         </h1>
         <p className="affiliation">
-          Graduate School of Information Science and Technology, The University
-          of Tokyo
-        </p>
+            Engineering @🔮 in 🗼</p>
 
         <hr style={{ marginTop: "25px", marginBottom: "25px" }} />
 
         <h2 id="about">About me</h2>
         <p>
-          I am a Ph.D. student in Computer Science at the University
-          of Tokyo. I am interested in training dynamics and generalization of
-          deep neural networks, particularly through statistical analysis and
-          visualization. I am fortunate to be advised by{" "}
-          <a
-            className="link"
-            href="https://www.i.u-tokyo.ac.jp/greeting_e.shtml"
-          >
-            Reiji Suda
+          Hi! I am a member of <a className="link" href="https://www.palantir.com" target="_blank" rel="noopener noreferrer">
+            🔮
           </a>
-          .
+          's engineering team in Tokyo. Prior to my current position, I was a
+          Ph.D. student in Computer Science at the University of Tokyo with a
+          concentration on training dynamics and generalization of deep neural
+          networks.
           <br />
-          Aside from my academia life, I am working as an intern (as of Dec.
-          2020) at Microsoft CELA, coordinating several philanthropic projects
-          with F500 customers focusing on leveraging AI for public health and
-          environment reservation.
-          <br />
-          In my free time, I enjoy reading (especially on Psychology,
-          Philosophy, Science/Technology), music (singing/acapella/piano, music composing), workout, and exploring the city by
-          foot/bike. I speak English, Japanese, and Mandarin fluently.
+          In my free time, I enjoy reading (especially about Psychology/Philosophy/Science), listening to audiobooks, playing music (singing/piano/composing), and taking long walks.
+          <br />I speak English, Japanese, and Mandarin fluently.
         </p>
 
         <hr style={{ marginTop: "25px", marginBottom: "25px" }} />
@@ -116,6 +106,8 @@ class About extends Component {
                   <a
                     className="link"
                     href="https://proceedings.neurips.cc/paper/2020"
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
                     <strong>NeurIPS 2020 </strong>
                   </a>
@@ -123,6 +115,8 @@ class About extends Component {
                   <a
                     className="link"
                     href="https://papers.nips.cc/paper/2020/hash/f0682320ccbbb1f1fb1e795de5e5639a-Abstract.html"
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
                     Paper
                   </a>
@@ -130,17 +124,21 @@ class About extends Component {
                   <a
                     className="link"
                     href="https://github.com/ihsuy/Train-by-Reconnect/blob/main/NeurIPS%20Poster.pdf"
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
                     Poster
                   </a>
                   ] [
-                  <a className="link" href="https://arxiv.org/abs/2003.02570">
+                  <a className="link" href="https://arxiv.org/abs/2003.02570" target="_blank" rel="noopener noreferrer">
                     arXiv
                   </a>
                   ] [
                   <a
                     className="link"
                     href="https://github.com/ihsuy/Train-by-Reconnect"
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
                     Project Page
                   </a>
@@ -148,6 +146,8 @@ class About extends Component {
                   <a
                     className="link"
                     href="https://paperswithcode.com/paper/permute-to-train-a-new-dimension-to-training"
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
                     Paper With Code
                   </a>
@@ -157,6 +157,8 @@ class About extends Component {
                   <a
                     className="link"
                     href="https://paperswithcode.com/paper/permute-to-train-a-new-dimension-to-training"
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
                     <Image
                       src="https://github.com/ihsuy/Train-by-Reconnect/blob/main/Images/connect.png?raw=true"
@@ -179,17 +181,17 @@ class About extends Component {
                   </strong>
                   <br />
                   PrePrint [
-                  <a className="link" href="https://arxiv.org/abs/2003.02570v4">
+                  <a className="link" href="https://arxiv.org/abs/2003.02570v4" target="_blank" rel="noopener noreferrer"> 
                     arXiv
                   </a>
                   ][
-                  <a className="link" href="https://github.com/ihsuy/SGPerm">
+                  <a className="link" href="https://github.com/ihsuy/SGPerm" target="_blank" rel="noopener noreferrer">
                     Project Page
                   </a>
                   ]
                 </Col>
                 <Col sm={4}>
-                  <a className="link" href="https://arxiv.org/abs/2003.02570v4">
+                  <a className="link" href="https://arxiv.org/abs/2003.02570v4" target="_blank" rel="noopener noreferrer">
                     <Image
                       src="https://github.com/ihsuy/SGPerm/blob/main/img/puzzle.png?raw=true"
                       style={{ height: "9vh" }}
@@ -211,6 +213,8 @@ class About extends Component {
                   <a
                     className="link"
                     href="https://sites.google.com/site/atathpsc/07-previous-conf/2018"
+                    target="_blank" 
+                    rel="noopener noreferrer"
                   >
                     ATAT in HPSC (2018)
                   </a>
@@ -233,7 +237,7 @@ class About extends Component {
         <Row>
           <Col sm={8}>
             <h3>
-              <a className="link" href="https://github.com/ihsuy/Typon">
+              <a className="link" href="https://github.com/ihsuy/Typon" target="_blank" rel="noopener noreferrer">
                 Typon
               </a>
             </h3>
@@ -245,14 +249,14 @@ class About extends Component {
             </p>
             <p>
               For more details, please see [
-              <a className="link" href="https://github.com/ihsuy/Typon">
+              <a className="link" href="https://github.com/ihsuy/Typon" target="_blank" rel="noopener noreferrer">
                 Project Page
               </a>
               ].
             </p>
           </Col>
           <Col sm={4}>
-            <a className="link" href="https://github.com/ihsuy/Typon">
+            <a className="link" href="https://github.com/ihsuy/Typon" target="_blank" rel="noopener noreferrer">
               <Image
                 src="https://github.com/ihsuy/Typon/raw/master/demo_gif/logo2.png"
                 style={{ width: "14vh", marginTop: "0pt" }}
@@ -272,8 +276,8 @@ class About extends Component {
               Tokyo, March 2020
             </li>
             <li>
-              Bachelor of Science, University of
-              Illinois at Urbana-Champaign, May 2017
+              Bachelor of Science, University of Illinois at Urbana-Champaign,
+              May 2017
             </li>
           </ul>
         </div>
@@ -286,7 +290,7 @@ class About extends Component {
             <li>E-mail: yushi621 [at] is.s.u-tokyo.ac.jp</li>
             <li>
               GitHub:{" "}
-              <a className="link" href="https://github.com/ihsuy">
+              <a className="link" href="https://github.com/ihsuy" target="_blank" rel="noopener noreferrer">
                 @ihsuy
               </a>
             </li>
